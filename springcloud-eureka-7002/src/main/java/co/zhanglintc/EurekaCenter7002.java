3e24095cde321f7bb361628b68f66eacb6cdfb74
zhanglintc <zhanglintc623@gmail.com>
2024-01-22 15:18:31 +0800

    eureka clusters

---
+++ springcloud-eureka-7002/src/main/java/co/zhanglintc/EurekaCenter7002.java
@@ -0,0 +1,13 @@
+package co.zhanglintc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaCenter7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaCenter7002.class, args);
+    }
+}
