9ab7eaaea78f2903537ca8b83ea754228e49ca2b
zhanglintc <zhanglintc623@gmail.com>
2024-01-22 13:01:59 +0800

    eureka client

--- springcloud-provider-8001/src/main/java/co/zhanglintc/SpringCloudProvider8001.java
+++ springcloud-provider-8001/src/main/java/co/zhanglintc/SpringCloudProvider8001.java
@@ -2,8 +2,10 @@ package co.zhanglintc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringCloudProvider8001 {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudProvider8001.class, args);
