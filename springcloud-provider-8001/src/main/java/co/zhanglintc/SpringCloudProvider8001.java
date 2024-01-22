735821a05413fd8ceff5747366f6a5080db56620
zhanglintc <zhanglintc623@gmail.com>
2024-01-22 11:06:25 +0800

    provider

---
+++ springcloud-provider-8001/src/main/java/co/zhanglintc/SpringCloudProvider8001.java
@@ -0,0 +1,11 @@
+package co.zhanglintc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudProvider8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudProvider8001.class, args);
+    }
+}
