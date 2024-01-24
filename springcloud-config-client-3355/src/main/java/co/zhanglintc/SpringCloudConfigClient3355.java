0cc437e145ab8bba136ccbb9f9f6e05d9f7ca761
zhanglintc <zhanglintc623@gmail.com>
2024-01-24 16:13:47 +0800

    3355

---
+++ springcloud-config-client-3355/src/main/java/co/zhanglintc/SpringCloudConfigClient3355.java
@@ -0,0 +1,11 @@
+package co.zhanglintc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClient3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigClient3355.class, args);
+    }
+}
---
+++ springcloud-config-client-3355/src/main/java/co/zhanglintc/controller/ConfigClientController.java
@@ -0,0 +1,34 @@
+package co.zhanglintc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+public class ConfigClientController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Autowired
+    private Environment env;
+
+    @RequestMapping("/")
+    public Object index() {
+        Map<String, Object> restResult = new LinkedHashMap<>();
+
+        restResult.put("serverPort", serverPort);
+        restResult.put("applicationName", applicationName);
+        restResult.put("spring.cloud.config.uri", env.getProperty("spring.cloud.config.uri"));
+        restResult.put("defaultValue", env.getProperty("notExist", "someDefaultValue"));
+
+        return restResult;
+    }
+}
